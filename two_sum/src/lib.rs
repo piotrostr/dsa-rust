bc5f2e3bccd23376a57f6c2be315937d345e97f5
Piotr Ostrowski <piotrostr@google.com>
2022-11-07 14:06:39 +0000

    pass with a two-pass solution

--- two_sum/src/lib.rs
+++ two_sum/src/lib.rs
@@ -1,7 +1,27 @@
+use std::collections::HashMap;
+
 pub struct Solution {}
 
 impl Solution {
     pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        // dump the numbers into a map
+        let mut map = HashMap::new();
+        for (index, num) in nums.iter().enumerate() {
+            map.insert(num, index);
+        }
+
+        for (index, num) in nums.iter().enumerate() {
+            let complement = target - num;
+            match map.get(&complement) {
+                Some(complement_index) => {
+                    if *complement_index != index {
+                        return vec![index as i32, *complement_index as i32];
+                    }
+                }
+                None => {}
+            }
+        }
+
         return vec![];
     }
 }
