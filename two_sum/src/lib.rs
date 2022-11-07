a34ff14555cdd5580c1d84a52170aa5e841be3c0
Piotr Ostrowski <piotrostr@google.com>
2022-11-07 14:24:12 +0000

    optimize the solution to do a single pass, sort the tests got/want to ensure that order is not relevant as long as Vec content is same

--- two_sum/src/lib.rs
+++ two_sum/src/lib.rs
@@ -4,24 +4,15 @@ pub struct Solution {}
 
 impl Solution {
     pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-        // dump the numbers into a map
-        let mut map = HashMap::new();
+        let mut map: HashMap<i32, i32> = HashMap::new();
         for (index, num) in nums.iter().enumerate() {
-            map.insert(num, index);
-        }
-
-        for (index, num) in nums.iter().enumerate() {
-            let complement = target - num;
-            match map.get(&complement) {
-                Some(complement_index) => {
-                    if *complement_index != index {
-                        return vec![index as i32, *complement_index as i32];
-                    }
+            match map.get(&(target - *num)) {
+                Some(&complement_index) => {
+                    return vec![index as i32, complement_index];
                 }
-                None => {}
-            }
+                None => map.insert(*num, index as i32),
+            };
         }
-
         return vec![];
     }
 }
@@ -34,8 +25,10 @@ mod tests {
     fn example_1() {
         let nums = vec![2, 7, 11, 15];
         let target = 9;
-        let got = Solution::two_sum(nums, target);
-        let want = [0, 1];
+        let mut got = Solution::two_sum(nums, target);
+        let mut want = [0, 1];
+        got.sort();
+        want.sort();
         assert_eq!(got, want);
     }
 
@@ -43,8 +36,10 @@ mod tests {
     fn example_2() {
         let nums = vec![3, 2, 4];
         let target = 6;
-        let got = Solution::two_sum(nums, target);
-        let want = [1, 2];
+        let mut got = Solution::two_sum(nums, target);
+        let mut want = [1, 2];
+        got.sort();
+        want.sort();
         assert_eq!(got, want);
     }
 
@@ -52,8 +47,10 @@ mod tests {
     fn example_3() {
         let nums = vec![3, 3];
         let target = 6;
-        let got = Solution::two_sum(nums, target);
-        let want = [0, 1];
+        let mut got = Solution::two_sum(nums, target);
+        let mut want = [0, 1];
+        got.sort();
+        want.sort();
         assert_eq!(got, want);
     }
 }
