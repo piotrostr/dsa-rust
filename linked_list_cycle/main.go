fdfe061d7984977acc14543705154099b27a0993
Piotr Ostrowski <piotrostr@google.com>
2022-11-09 15:32:20 +0000

    pass tests

--- linked_list_cycle/main.go
+++ linked_list_cycle/main.go
@@ -5,6 +5,27 @@ type ListNode struct {
 	Next *ListNode
 }
 
+// The solution to the problem seems very simple, just unpack the list and check
+// if the index `pos` exists in the array
 func HasCycle(head *ListNode, pos int) bool {
-	return false
+	var arr []int
+	for {
+		arr = append(arr, head.Val)
+		head = head.Next
+		if head == nil {
+			break
+		}
+	}
+
+	// no negative indices in the array allowed
+	if pos < 0 {
+		return false
+	}
+
+	// the index cannot be outside of the array
+	if (pos - 1) >= len(arr) {
+		return false
+	}
+
+	return true
 }
