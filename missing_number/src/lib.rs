99f8c5851099b99c0cb8e9105705379c2e25c5b3
Piotr Ostrowski <piotrostr@google.com>
2022-11-10 15:57:51 +0100

    solve the problem, mess with cyclic sort

--- missing_number/src/lib.rs
+++ missing_number/src/lib.rs
@@ -2,7 +2,30 @@ pub struct Solution;
 
 impl Solution {
     pub fn missing_number(nums: Vec<i32>) -> i32 {
+        let mut nums = nums.clone();
+
+        cyclic_sort(&mut nums);
+
+        for (index, num) in nums.iter().enumerate() {
+            if index as i32 != *num {
+                return index as i32;
+            }
+        }
         0
+    }
+}
+
+pub fn cyclic_sort(nums: &mut Vec<i32>) {
+    let mut i: usize = 0;
+    while i < nums.len() {
+        let j = nums[i] as usize;
+        if nums[i] < nums.len() as i32 && nums[i] != nums[j] {
+            let temp = nums[j];
+            nums[j] = nums[i];
+            nums[i] = temp;
+        } else {
+            i += 1;
+        }
     }
 }
 
