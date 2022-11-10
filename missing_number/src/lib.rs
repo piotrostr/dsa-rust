5b167b2853f95b844ae751566bbd8dc074f64ef2
Piotr Ostrowski <piotrostr@google.com>
2022-11-10 17:22:40 +0100

    add another, simpler solution with map reduce

--- missing_number/src/lib.rs
+++ missing_number/src/lib.rs
@@ -2,9 +2,16 @@ pub struct Solution;
 
 impl Solution {
     pub fn missing_number(nums: Vec<i32>) -> i32 {
+        let n = nums.len();
+        let required_sum = (0..=n).into_iter().reduce(|x, y| x + y).unwrap();
+        let sum = nums.iter().map(|x| *x).reduce(|x, y| x + y).unwrap();
+        required_sum as i32 - sum
+    }
+
+    pub fn missing_number_with_sort(nums: Vec<i32>) -> i32 {
         let mut nums = nums.clone();
 
-        cyclic_sort(&mut nums);
+        cyclic_sort(&mut nums); // or nums.sort();
 
         for (index, num) in nums.iter().enumerate() {
             if index as i32 != *num {
@@ -17,9 +24,10 @@ impl Solution {
 
 pub fn cyclic_sort(nums: &mut Vec<i32>) {
     let mut i: usize = 0;
+    let n = nums.len() as i32;
     while i < nums.len() {
         let j = nums[i] as usize;
-        if nums[i] < nums.len() as i32 && nums[i] != nums[j] {
+        if nums[i] < n && nums[i] != nums[j] {
             let temp = nums[j];
             nums[j] = nums[i];
             nums[i] = temp;
