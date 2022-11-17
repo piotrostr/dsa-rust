dc18aa1502173d0df24296d7a54c27fddd15db7c
Piotr Ostrowski <piotrostr@google.com>
2022-11-17 16:22:26 +0000

    add more comments, add another testcase (failing) and pass it

--- flood_fill/main.go
+++ flood_fill/main.go
@@ -4,7 +4,13 @@ func FloodFill(image [][]int, sr int, sc int, color int) [][]int {
 	maxR := len(image) - 1    // max index of row
 	maxC := len(image[0]) - 1 // max index of col
 
-	starting := image[sr][sc]
+	// if the starting pixel is the right color - do nothing
+	if image[sr][sc] == color {
+		return image
+	}
+
+	// keep the starting number to later compare if the flood can spread
+	startingNum := image[sr][sc]
 
 	// set the color in order not to fall in endless recursion
 	image[sr][sc] = color
@@ -13,28 +19,28 @@ func FloodFill(image [][]int, sr int, sc int, color int) [][]int {
 
 	// dont want out of bounds row indices
 	if sr+1 <= maxR {
-		if starting == image[sr+1][sc] {
+		if startingNum == image[sr+1][sc] {
 			FloodFill(image, sr+1, sc, color)
 		}
 	}
 
 	// dont want negative row indices
 	if sr-1 >= 0 {
-		if starting == image[sr-1][sc] {
+		if startingNum == image[sr-1][sc] {
 			FloodFill(image, sr-1, sc, color)
 		}
 	}
 
 	// dont want out of bounds col indices
 	if sc+1 <= maxC {
-		if starting == image[sr][sc+1] {
+		if startingNum == image[sr][sc+1] {
 			FloodFill(image, sr, sc+1, color)
 		}
 	}
 
 	// dont want negative col indices
 	if sc-1 >= 0 {
-		if starting == image[sr][sc-1] {
+		if startingNum == image[sr][sc-1] {
 			FloodFill(image, sr, sc-1, color)
 		}
 	}
--- flood_fill/main_test.go
+++ flood_fill/main_test.go
@@ -24,3 +24,21 @@ func TestFloodFill(t *testing.T) {
 		t.Errorf("TestFloodFill: got %v want %v\n", got, want)
 	}
 }
+
+func TestFloodFillWithZeros(t *testing.T) {
+	image := [][]int{
+		{0, 0, 0},
+		{0, 0, 0},
+	}
+	want := [][]int{
+		{0, 0, 0},
+		{0, 0, 0},
+	}
+	sr := 0
+	sc := 0
+	color := 0
+	got := FloodFill(image, sr, sc, color)
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("TestFloodFill: got %v want %v\n", got, want)
+	}
+}
