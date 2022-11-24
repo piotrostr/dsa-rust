f4f9df76728ba24dca923aa6f3f5e7fe6dbc0d2e
Piotr Ostrowski <piotrostr@google.com>
2022-11-24 19:30:12 +0000

    improve the algorithm complexity, both linear and space

--- number_of_islands/main.go
+++ number_of_islands/main.go
@@ -1,17 +1,16 @@
 package main
 
-const One = 0b110001
-
 // how is this graph theory?
 //
 // the islands are graphs
 //
 // solution can be obtained through forming out all of the possible graphs of
 // ones in the grid
+//
+// search through all o the
 func numIslands(grid [][]byte) int {
 	// keep track of the already visited spots
 	// visitedMap := map[int]map[int]bool{}
-	visitedMap := makeMap(len(grid), len(grid[0]))
 	islandsCount := 0
 
 	// iterate through all of the squares but break if already visited
@@ -19,69 +18,43 @@ func numIslands(grid [][]byte) int {
 	for row := 0; row < len(grid); row++ {
 		for col := 0; col < len(grid[0]); col++ {
 			// for the first time if
-			if isValid(grid[row][col]) && !wasVisited(row, col, visitedMap) {
+			if grid[row][col] == '1' {
 				islandsCount += 1
 			}
 
-			// if it is not in the map it has not been visited yet run the DFS
-			// DFS will stop when it hits a zero, only visiting the island
-			dfs(grid, row, col, visitedMap)
+			// if it is not in the map it has not been visited yet
+			// run the DFS
+			// DFS will stop when it hits a zero, only visiting the
+			// island
+			dfs(grid, row, col)
 		}
 	}
 	return islandsCount
 }
 
-func makeMap(n int, m int) map[int]map[int]bool {
-	visitedMap := make(map[int]map[int]bool)
-	for i := 0; i < n; i++ {
-		visitedMap[i] = make(map[int]bool)
-		for j := 0; j < m; j++ {
-			visitedMap[i][j] = false
-		}
-	}
-	return visitedMap
-}
-
-func dfs(grid [][]byte, row int, col int, visitedMap map[int]map[int]bool) {
-	visited := wasVisited(row, col, visitedMap)
-	if visited {
+func dfs(grid [][]byte, row int, col int) {
+	if grid[row][col] != '1' {
 		return
 	}
 
-	visitedMap[row][col] = true
-
-	if !isValid(grid[row][col]) {
-		return
-	}
+	grid[row][col] = '#'
 
 	// spread around peers, check if won't go out of bounds though
+
 	// go left
 	if row-1 >= 0 {
-		dfs(grid, row-1, col, visitedMap)
+		dfs(grid, row-1, col)
 	}
 	// go right
 	if row+1 < len(grid) {
-		dfs(grid, row+1, col, visitedMap)
+		dfs(grid, row+1, col)
 	}
 	// go up
 	if col-1 >= 0 {
-		dfs(grid, row, col-1, visitedMap)
+		dfs(grid, row, col-1)
 	}
 	// go down
 	if col+1 < len(grid[0]) {
-		dfs(grid, row, col+1, visitedMap)
+		dfs(grid, row, col+1)
 	}
-}
-
-func wasVisited(row int, col int, visitedMap map[int]map[int]bool) bool {
-	visited, ok := visitedMap[row][col]
-	if !ok {
-		visited = false
-	}
-
-	return visited
-}
-
-func isValid(b byte) bool {
-	return b == One
 }
