253d66bd9220e9ef37ddd9cbbe6afd22b03a3fe0
Piotr Ostrowski <piotrostr@google.com>
2023-01-18 11:10:58 +0000

    pass the submission for the Permutations problem, this also solves Permutations II (same thing but with duplicates)

--- permutations/src/lib.rs
+++ permutations/src/lib.rs
@@ -1,8 +1,52 @@
+use std::collections::HashSet;
+
 pub struct Solution {}
 
 impl Solution {
     pub fn permute(nums: Vec<i32>) -> Vec<Vec<i32>> {
-        vec![]
+        let mut permutations: HashSet<Vec<i32>> = HashSet::new();
+        let target_length = nums.len();
+        let mut nums = nums;
+        let permutation = vec![];
+        for i in 0..nums.len() {
+            let mut permutation = permutation.clone();
+            // not sure how to go about below in good complexity
+            let x = nums.remove(i);
+            permutation.push(x);
+            bt(&mut permutations, nums.clone(), permutation, &target_length);
+            // and here; suboptimal
+            nums.insert(i, x);
+        }
+        let mut res: Vec<Vec<i32>> = permutations.iter().map(|item| item.to_owned()).collect();
+        res.sort();
+        res
+    }
+}
+
+pub fn bt(
+    permutations: &mut HashSet<Vec<i32>>,
+    nums: Vec<i32>,
+    permutation: Vec<i32>,
+    target_length: &usize,
+) {
+    if permutation.len() == *target_length {
+        permutations.insert(permutation);
+        return;
+    }
+    let mut nums = nums;
+    for i in 0..nums.len() {
+        // not sure how to go about below in good complexity
+        let x = nums.remove(i);
+        let mut permutation = permutation.clone().to_owned();
+        permutation.push(x);
+        bt(
+            permutations,
+            nums.clone(),
+            permutation.clone(),
+            target_length,
+        );
+        // and here; suboptimal
+        nums.insert(i, x);
     }
 }
 
---
+++ permutations/src/main.rs
@@ -0,0 +1,3 @@
+fn main() {
+    permutations::Solution::permute(vec![1, 2, 3]);
+}
